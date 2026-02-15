ce072bb7e96eebed71e1b6be7b50204733e7447e
Alex potter <alexpandiyan1914@gmail.com>
2026-02-15 14:12:51 +0530

    generate JWT token, implement Delete account logic, restrict access to register and login while logged in

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,48 @@ app.post('/login', (req, res) => {
             return res.status(401).json({ message: `Email or password invalid` });
         }
 
-        res.json({ message: `Login Sucessfull` });
+        const token  = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn:'10m'}  
+        );
+
+        res.status(200).json({
+            message:'Login sucessful',
+            token:token,
+            name:user.name
+        });
+    });
+});
+
+function verifyToken(req,res,next){
+    const token  = req.headers.authorization;
+
+    if(!token){
+        return res.status(403).json({message:`No token provided`});
+    }
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(401).json({message:`Invalid token`});
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
+
+app.delete('/delete-account', verifyToken, (req,res)=>{
+    const userId = req.user.id;
+
+    const sql = "DELETE FROM users WHERE id = ?";
+
+    db.query(sql,[userId],(err,result)=>{
+        if(err){
+            return res.status(500).json({message:`Server error`})
+        }
+
+        res.status(200).json({message:`Account Deleted Succesfully`});
     });
 });
 
